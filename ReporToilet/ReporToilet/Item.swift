c72816ea71c2f61a4df79df74745bcccfdb292b8
haejin park <jelly12paw@gmail.com>
2024-06-18 00:25:53 +0900

    [Init] Xcode init setting

---
+++ ReporToilet/ReporToilet/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  ReporToilet
+//
+//  Created by EVIE on 6/18/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
